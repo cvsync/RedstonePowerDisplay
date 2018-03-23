1765a59067ca76e3a082e7150bcce90d0b3bbe5e
MAEKAWA Masahide <maekawa@daemon-systems.org>
2018-03-23 22:11:50 +0900

    Display number font : M+ bitmap h12r.

--- README.md
+++ README.md
@@ -2,14 +2,18 @@
 The Minecraft resource pack ``Redstone Power Display'' , which replace redstone wire textures and display the redstone power level.
 
 ## Latest Version
-1.2.0
+1.3.0
 
 ## Variants
 RedstonePowerDisplay.zip - Overlay '0' when a redstone dust is not powered.
 
 RedstonePowerDisplay_NoPower0.zip - Don't show '0' when a redstone dust is not powered.
 
+mplus_h12r : font is M+ bitmap hlv 12r
+
+http://mplus-fonts.osdn.jp/mplus-bitmap-fonts/index.html
+
 ## For Java Edition users
-1.) Download RedstonePowerDisplay.zip or RedstonePowerDisplay_NoPower0.zip
+1.) Download zip file.
 
 2.) Put it into C:\Users\\\<Username\>\AppData\Roaming\.minecraft\resourcepacks
--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #! /bin/sh
 
 VER_MAJOR=1
-VER_MINOR=2
+VER_MINOR=3
 
 RESOURCE_PACK_NAME=RedstonePowerDisplay
-
-RESOURCE_PACK_FILE_JAVA=${RESOURCE_PACK_NAME}.zip
 
 SRCDIR=${RESOURCE_PACK_NAME}/assets/minecraft
 
@@ -15,6 +13,7 @@ TMPBASEDIR=${TMPDIR}/assets/minecraft
 #
 # for Java Edition
 #
+RESOURCE_PACK_FILE_JAVA=${RESOURCE_PACK_NAME}.zip
 rm -f ${RESOURCE_PACK_FILE_JAVA}
 rm -f -r ${TMPDIR}
 mkdir -p ${TMPDIR} ${TMPDIR}/assets ${TMPBASEDIR}
@@ -35,12 +34,14 @@ do
 	cat > ${TMPBASEDIR}/models/block/rs${_num}.json
 done
 # textures
-cp -R ${SRCDIR}/textures ${TMPBASEDIR}/
+mkdir -p ${TMPBASEDIR}/textures
+cp -R ${SRCDIR}/textures/blocks ${TMPBASEDIR}/textures/
 
 cp ${RESOURCE_PACK_NAME}/pack.png ${TMPDIR}/
 cat ${RESOURCE_PACK_NAME}/pack.mcmeta | \
 sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
 sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
+sed "s/XXXSUBSTRXXX//g" | \
 cat > ${TMPDIR}/pack.mcmeta
 (cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_JAVA} *)
 rm -f -r ${TMPDIR}
@@ -70,12 +71,89 @@ do
 	cat > ${TMPBASEDIR}/models/block/rs${_num}.json
 done
 # textures
-cp -R ${SRCDIR}/textures ${TMPBASEDIR}/
+mkdir -p ${TMPBASEDIR}/textures
+cp -R ${SRCDIR}/textures/blocks ${TMPBASEDIR}/textures/
 
 cp ${RESOURCE_PACK_NAME}/pack_no_power_0.png ${TMPDIR}/pack.png
 cat ${RESOURCE_PACK_NAME}/pack.mcmeta | \
 sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
 sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
+sed "s/XXXSUBSTRXXX//g" | \
+cat > ${TMPDIR}/pack.mcmeta
+(cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_JAVA} *)
+rm -f -r ${TMPDIR}
+
+#
+# for Java Edition (M+ hlv 12r)
+#
+RESOURCE_PACK_FILE_JAVA=${RESOURCE_PACK_NAME}_mplus_h12r.zip
+rm -f ${RESOURCE_PACK_FILE_JAVA}
+rm -f -r ${TMPDIR}
+mkdir -p ${TMPDIR} ${TMPDIR}/assets ${TMPBASEDIR}
+# blockstates
+mkdir -p ${TMPBASEDIR}/blockstates
+cat ${SRCDIR}/blockstates/redstone_wire.json | \
+cat > ${TMPBASEDIR}/blockstates/redstone_wire.json
+# models
+mkdir -p ${TMPBASEDIR}/models ${TMPBASEDIR}/models/block
+for _file in redstone_dot rs
+do
+	cp ${SRCDIR}/models/block/${_file}.json ${TMPBASEDIR}/models/block/
+done
+for _num in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15
+do
+	cat ${SRCDIR}/models/block/rsXX.json | \
+	sed "s/XXNUMXX/${_num}/g" | \
+	cat > ${TMPBASEDIR}/models/block/rs${_num}.json
+done
+# textures
+mkdir -p ${TMPBASEDIR}/textures
+cp -R ${SRCDIR}/textures/blocks ${TMPBASEDIR}/textures/
+cp ${SRCDIR}/textures/mplus_h12r/*.png ${TMPBASEDIR}/textures/blocks/
+
+cp ${RESOURCE_PACK_NAME}/pack.png ${TMPDIR}/
+cat ${RESOURCE_PACK_NAME}/pack.mcmeta | \
+sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
+sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
+sed "s/XXXSUBSTRXXX/(M+ h12r) /g" | \
+cat > ${TMPDIR}/pack.mcmeta
+(cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_JAVA} *)
+rm -f -r ${TMPDIR}
+
+#
+# for Java Edition (w/o 0) (M+ hlv 12r)
+#
+RESOURCE_PACK_FILE_JAVA=${RESOURCE_PACK_NAME}_NoPower0_mplus_h12r.zip
+rm -f ${RESOURCE_PACK_FILE_JAVA}
+rm -f -r ${TMPDIR}
+mkdir -p ${TMPDIR} ${TMPDIR}/assets ${TMPBASEDIR}
+# blockstates
+mkdir -p ${TMPBASEDIR}/blockstates
+cat ${SRCDIR}/blockstates/redstone_wire.json | \
+grep -v "rs00" | \
+cat > ${TMPBASEDIR}/blockstates/redstone_wire.json
+# models
+mkdir -p ${TMPBASEDIR}/models ${TMPBASEDIR}/models/block
+for _file in redstone_dot rs
+do
+	cp ${SRCDIR}/models/block/${_file}.json ${TMPBASEDIR}/models/block/
+done
+for _num in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15
+do
+	cat ${SRCDIR}/models/block/rsXX.json | \
+	sed "s/XXNUMXX/${_num}/g" | \
+	cat > ${TMPBASEDIR}/models/block/rs${_num}.json
+done
+# textures
+mkdir -p ${TMPBASEDIR}/textures
+cp -R ${SRCDIR}/textures/blocks ${TMPBASEDIR}/textures/
+cp ${SRCDIR}/textures/mplus_h12r/*.png ${TMPBASEDIR}/textures/blocks/
+
+cp ${RESOURCE_PACK_NAME}/pack_no_power_0.png ${TMPDIR}/pack.png
+cat ${RESOURCE_PACK_NAME}/pack.mcmeta | \
+sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
+sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
+sed "s/XXXSUBSTRXXX/(M+ h12r) /g" | \
 cat > ${TMPDIR}/pack.mcmeta
 (cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_JAVA} *)
 rm -f -r ${TMPDIR}

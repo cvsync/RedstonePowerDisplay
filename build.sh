7ce8131b4692e7fbb0f1e5c6e63e045647d679c1
MAEKAWA Masahide <maekawa@daemon-systems.org>
2018-07-26 21:05:18 +0900

    1.4.0 for Java Edition 1.13 users.

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #! /bin/sh
 
 VER_MAJOR=1
-VER_MINOR=3
+VER_MINOR=4
 
 RESOURCE_PACK_NAME=RedstonePowerDisplay
+PACK_FORMAT=4
 
 SRCDIR=${RESOURCE_PACK_NAME}/assets/minecraft
 
 TMPDIR=./tmp
 TMPBASEDIR=${TMPDIR}/assets/minecraft
+
+TEXTURES_BLOCKTARGET=block
+TEXTURES_BLOCKDIR=${TMPBASEDIR}/textures/${TEXTURES_BLOCKTARGET}
 
 #
 # for Java Edition
@@ -25,20 +29,24 @@ cat > ${TMPBASEDIR}/blockstates/redstone_wire.json
 mkdir -p ${TMPBASEDIR}/models ${TMPBASEDIR}/models/block
 for _file in redstone_dot rs
 do
-	cp ${SRCDIR}/models/block/${_file}.json ${TMPBASEDIR}/models/block/
+	cat ${SRCDIR}/models/block/${_file}.json | \
+	sed "s/XXBLOCKDIRXX/${TEXTURES_BLOCKTARGET}/g" | \
+	cat > ${TMPBASEDIR}/models/block/${_file}.json
 done
 for _num in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15
 do
 	cat ${SRCDIR}/models/block/rsXX.json | \
+	sed "s/XXBLOCKDIRXX/${TEXTURES_BLOCKTARGET}/g" | \
 	sed "s/XXNUMXX/${_num}/g" | \
 	cat > ${TMPBASEDIR}/models/block/rs${_num}.json
 done
 # textures
-mkdir -p ${TMPBASEDIR}/textures
-cp -R ${SRCDIR}/textures/blocks ${TMPBASEDIR}/textures/
+mkdir -p ${TMPBASEDIR}/textures ${TEXTURES_BLOCKDIR}
+cp ${SRCDIR}/textures/blocks/* ${TEXTURES_BLOCKDIR}/
 
 cp ${RESOURCE_PACK_NAME}/pack.png ${TMPDIR}/
 cat ${RESOURCE_PACK_NAME}/pack.mcmeta | \
+sed "s/XXXPACKFORMATXXX/${PACK_FORMAT}/g" | \
 sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
 sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
 sed "s/XXXSUBSTRXXX//g" | \
@@ -62,20 +70,24 @@ cat > ${TMPBASEDIR}/blockstates/redstone_wire.json
 mkdir -p ${TMPBASEDIR}/models ${TMPBASEDIR}/models/block
 for _file in redstone_dot rs
 do
-	cp ${SRCDIR}/models/block/${_file}.json ${TMPBASEDIR}/models/block/
+	cat ${SRCDIR}/models/block/${_file}.json | \
+	sed "s/XXBLOCKDIRXX/${TEXTURES_BLOCKTARGET}/g" | \
+	cat > ${TMPBASEDIR}/models/block/${_file}.json
 done
 for _num in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15
 do
 	cat ${SRCDIR}/models/block/rsXX.json | \
+	sed "s/XXBLOCKDIRXX/${TEXTURES_BLOCKTARGET}/g" | \
 	sed "s/XXNUMXX/${_num}/g" | \
 	cat > ${TMPBASEDIR}/models/block/rs${_num}.json
 done
 # textures
-mkdir -p ${TMPBASEDIR}/textures
-cp -R ${SRCDIR}/textures/blocks ${TMPBASEDIR}/textures/
+mkdir -p ${TMPBASEDIR}/textures ${TEXTURES_BLOCKDIR}
+cp ${SRCDIR}/textures/blocks/* ${TEXTURES_BLOCKDIR}/
 
 cp ${RESOURCE_PACK_NAME}/pack_no_power_0.png ${TMPDIR}/pack.png
 cat ${RESOURCE_PACK_NAME}/pack.mcmeta | \
+sed "s/XXXPACKFORMATXXX/${PACK_FORMAT}/g" | \
 sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
 sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
 sed "s/XXXSUBSTRXXX//g" | \
@@ -98,21 +110,25 @@ cat > ${TMPBASEDIR}/blockstates/redstone_wire.json
 mkdir -p ${TMPBASEDIR}/models ${TMPBASEDIR}/models/block
 for _file in redstone_dot rs
 do
-	cp ${SRCDIR}/models/block/${_file}.json ${TMPBASEDIR}/models/block/
+	cat ${SRCDIR}/models/block/${_file}.json | \
+	sed "s/XXBLOCKDIRXX/${TEXTURES_BLOCKTARGET}/g" | \
+	cat > ${TMPBASEDIR}/models/block/${_file}.json
 done
 for _num in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15
 do
 	cat ${SRCDIR}/models/block/rsXX.json | \
+	sed "s/XXBLOCKDIRXX/${TEXTURES_BLOCKTARGET}/g" | \
 	sed "s/XXNUMXX/${_num}/g" | \
 	cat > ${TMPBASEDIR}/models/block/rs${_num}.json
 done
 # textures
-mkdir -p ${TMPBASEDIR}/textures
-cp -R ${SRCDIR}/textures/blocks ${TMPBASEDIR}/textures/
-cp ${SRCDIR}/textures/mplus_h12r/*.png ${TMPBASEDIR}/textures/blocks/
+mkdir -p ${TMPBASEDIR}/textures ${TEXTURES_BLOCKDIR}
+cp ${SRCDIR}/textures/blocks/* ${TEXTURES_BLOCKDIR}/
+cp ${SRCDIR}/textures/mplus_h12r/*.png ${TEXTURES_BLOCKDIR}/
 
 cp ${RESOURCE_PACK_NAME}/pack.png ${TMPDIR}/
 cat ${RESOURCE_PACK_NAME}/pack.mcmeta | \
+sed "s/XXXPACKFORMATXXX/${PACK_FORMAT}/g" | \
 sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
 sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
 sed "s/XXXSUBSTRXXX/(M+ h12r) /g" | \
@@ -136,21 +152,25 @@ cat > ${TMPBASEDIR}/blockstates/redstone_wire.json
 mkdir -p ${TMPBASEDIR}/models ${TMPBASEDIR}/models/block
 for _file in redstone_dot rs
 do
-	cp ${SRCDIR}/models/block/${_file}.json ${TMPBASEDIR}/models/block/
+	cat ${SRCDIR}/models/block/${_file}.json | \
+	sed "s/XXBLOCKDIRXX/${TEXTURES_BLOCKTARGET}/g" | \
+	cat > ${TMPBASEDIR}/models/block/${_file}.json
 done
 for _num in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15
 do
 	cat ${SRCDIR}/models/block/rsXX.json | \
+	sed "s/XXBLOCKDIRXX/${TEXTURES_BLOCKTARGET}/g" | \
 	sed "s/XXNUMXX/${_num}/g" | \
 	cat > ${TMPBASEDIR}/models/block/rs${_num}.json
 done
 # textures
-mkdir -p ${TMPBASEDIR}/textures
-cp -R ${SRCDIR}/textures/blocks ${TMPBASEDIR}/textures/
-cp ${SRCDIR}/textures/mplus_h12r/*.png ${TMPBASEDIR}/textures/blocks/
+mkdir -p ${TMPBASEDIR}/textures ${TEXTURES_BLOCKDIR}
+cp ${SRCDIR}/textures/blocks/* ${TEXTURES_BLOCKDIR}/
+cp ${SRCDIR}/textures/mplus_h12r/*.png ${TEXTURES_BLOCKDIR}/
 
 cp ${RESOURCE_PACK_NAME}/pack_no_power_0.png ${TMPDIR}/pack.png
 cat ${RESOURCE_PACK_NAME}/pack.mcmeta | \
+sed "s/XXXPACKFORMATXXX/${PACK_FORMAT}/g" | \
 sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
 sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
 sed "s/XXXSUBSTRXXX/(M+ h12r) /g" | \
